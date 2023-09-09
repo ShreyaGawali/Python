e704f4c3a30fb707655ac21285d56834a111d689
ShreyaGawali <142747109+ShreyaGawali@users.noreply.github.com>
2023-09-10 01:21:09 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,9 @@
+# my first testing program of creating class in python
+
+class Test:
+    def display(self):
+        print("testing instance method of python class")
+
+#main program
+ob= Test()                       #object creation
+ob.display()
