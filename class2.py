099cdc55a69d0b24801c78de19ec1e57d4e6cadf
ShreyaGawali <142747109+ShreyaGawali@users.noreply.github.com>
2023-09-10 01:21:46 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,11 @@
+
+class demo:
+    def display(self):
+        print("we are in instance method")
+    def show(self):
+        print("we are in show method")
+#main method
+
+d=demo()          #object creation
+d.display()
+d.show()
